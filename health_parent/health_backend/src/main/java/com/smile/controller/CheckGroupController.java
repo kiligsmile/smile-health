0150cdfaadf24abd6e0466f13e77595ee2ded9b6
kiligsmile <kiligsmile@163.com>
2023-05-21 23:12:01 +0800

    检查组页面

--- health_parent/health_backend/src/main/java/com/smile/controller/CheckGroupController.java
+++ health_parent/health_backend/src/main/java/com/smile/controller/CheckGroupController.java
@@ -6,10 +6,13 @@ import com.smile.entity.PageResult;
 import com.smile.entity.QueryPageBean;
 import com.smile.entity.Result;
 import com.smile.pojo.CheckGroup;
+import com.smile.pojo.CheckItem;
 import com.smile.service.CheckGroupService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //检查组管理
 @RestController
@@ -34,5 +37,64 @@ public class CheckGroupController {
     public PageResult findPage(@RequestBody QueryPageBean queryPageBean){
         PageResult pageResult = checkGroupService.pageQuery(queryPageBean);
         return pageResult;
+    }
+
+    // 编辑检查项
+    @RequestMapping("/edit")
+    public Result edit(@RequestBody CheckGroup checkGroup,Integer[] checkitemIds){
+        try {
+            checkGroupService.edit(checkGroup,checkitemIds);
+        }catch (Exception e){
+            e.printStackTrace();;
+            return new Result(false, MessageConstant.EDIT_CHECKGROUP_FAIL);
+        }
+        return new Result(true,MessageConstant.EDIT_CHECKGROUP_SUCCESS);
+    }
+
+    // 编辑检查项
+    @RequestMapping("/findById")
+    public Result findById(Integer id){
+        try {
+            CheckGroup checkGroup= checkGroupService.findById(id);
+            return new Result(true,MessageConstant.QUERY_CHECKITEM_SUCCESS,checkGroup);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false, MessageConstant.QUERY_CHECKITEM_FAIL);
+        }
+    }
+
+    // 编辑检查项
+    @RequestMapping("/findAll")
+    public Result findAll(){
+        try {
+            List<CheckGroup> list= checkGroupService.findAll();
+            return new Result(true,MessageConstant.QUERY_CHECKGROUP_SUCCESS,list);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false, MessageConstant.QUERY_CHECKGROUP_FAIL);
+        }
+    }
+
+    @RequestMapping("/findCheckItemIdsByCheckGroupId")
+    public Result findCheckItemIdsByCheckGroupId(Integer id){
+        try {
+            List<Integer> checkitemIds= checkGroupService.findCheckItemIdsByCheckGroupId(id);
+            return new Result(true,MessageConstant.QUERY_CHECKGROUP_SUCCESS,checkitemIds);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false, MessageConstant.QUERY_CHECKGROUP_FAIL);
+        }
+    }
+
+    // 删除检查项
+    @RequestMapping("/delete")
+    public Result delete(Integer id){
+        try {
+            checkGroupService.deleteById(id);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false, MessageConstant.DELETE_CHECKGROUP_FAIL);
+        }
+        return new Result(true,MessageConstant.DELETE_CHECKGROUP_SUCCESS  );
     }
 }
--- health_parent/health_interface/src/main/java/com/smile/service/CheckGroupService.java
+++ health_parent/health_interface/src/main/java/com/smile/service/CheckGroupService.java
@@ -3,8 +3,16 @@ package com.smile.service;
 import com.smile.entity.PageResult;
 import com.smile.entity.QueryPageBean;
 import com.smile.pojo.CheckGroup;
+import com.smile.pojo.CheckItem;
+
+import java.util.List;
 
 public interface CheckGroupService {
     public void add(CheckGroup checkGroup, Integer[] checkitemIds);
     public PageResult pageQuery(QueryPageBean queryPageBean);
+    public void edit(CheckGroup checkGroup,Integer[] checkitemIds);
+    public CheckGroup findById(Integer id);
+    public List<CheckGroup> findAll();
+    public List<Integer> findCheckItemIdsByCheckGroupId(Integer id);
+    public void deleteById(Integer id);
 }
