74222ad4dede3dd0b6ac20632294f5d852905f87
kiligsmile <kiligsmile@163.com>
2023-09-11 11:00:08 +0800

    修复邮箱验证码

--- health_parent/health_common/src/main/java/com/smile/utils/SMSUtils.java
+++ health_parent/health_common/src/main/java/com/smile/utils/SMSUtils.java
@@ -41,7 +41,8 @@ public class SMSUtils {
             prop.put("mail.smtp.ssl.socketFactory", sf);
             Session session = Session.getInstance(prop);
             Transport ts = session.getTransport();
-            ts.connect("smtp.qq.com", "1636625079@qq.com", "hqmvzsubymdvggaj");
+            ts.connect("smtp.qq.com", "1636625079@qq.com", "hdsjiouhrneyefbj");
+//            ts.connect("imap.163.com", "kiligsmile@163.com", "GJASKOQEXVZXZXBF");
 //            邮件接收者
             Message message = createSimpleMail(session, receiveMail,code);
             ts.sendMessage(message, message.getAllRecipients());
